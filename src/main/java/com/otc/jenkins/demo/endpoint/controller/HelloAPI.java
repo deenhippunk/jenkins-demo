54e0336c5946f78ffac9d6fd8c1e41406750dff8
deenhippunk <deenhippunk@gmail.com>
2024-05-27 16:43:04 +0700

    init

---
+++ src/main/java/com/otc/jenkins/demo/endpoint/controller/HelloAPI.java
@@ -0,0 +1,15 @@
+package com.otc.jenkins.demo.endpoint.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/greeting")
+public class HelloAPI 
+{
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
+}
