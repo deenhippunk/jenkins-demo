3281052b90a0722caf5a556cfb978301584f8c9d
deenhippunk <deenhippunk@gmail.com>
2024-06-05 00:05:10 +0700

    Implement Hi API

--- src/main/java/com/otc/jenkins/demo/endpoint/controller/HiAPI.java
+++ src/main/java/com/otc/jenkins/demo/endpoint/controller/HiAPI.java
@@ -1,5 +1,14 @@
 package com.otc.jenkins.demo.endpoint.controller;
 
-public class HiAPI {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/greeting")
+public class HiAPI {
+	@GetMapping("/hi")
+	public String hello() {
+		return "HI";
+	}
 }
